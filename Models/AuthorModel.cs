affe5427b634d6ee2de0cc2d02d9d3d1293fcd1d
BurakTeli <tellibrk216@gmail.com>
2025-03-09 21:42:17 +0300

    Library Management System

--- Models/AuthorModel.cs
+++
@@ -1,7 +0,0 @@
-public class Author
-{
-    public int Id { get; set; } // Unique identifier
-    public string FirstName { get; set; } // Author's first name
-    public string LastName { get; set; } // Author's last name
-    public DateTime DateOfBirth { get; set; } // Author's birth date
-}
--- Week9/ViewModels/AuthorViewModel.cs
+++
@@ -1,6 +0,0 @@
-public class AuthorViewModel
-{
-    public int Id { get; set; }
-    public string FullName { get; set; } // Full name of the author
-    public DateTime DateOfBirth { get; set; } 
-}
